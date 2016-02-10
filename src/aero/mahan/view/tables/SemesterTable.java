6ff546825ed58b7c46f72cce12587563c9e28cd1
9316173 <azmoon2012@gmail.com>
2016-02-10 10:26:30 +0330

    The model Readed data from Db

--- src/aero/mahan/view/tables/SemesterTable.java
+++ src/aero/mahan/view/tables/SemesterTable.java
@@ -4,18 +4,27 @@ import aero.mahan.model.Semester;
 import aero.mahan.view.tables.models.SemesterTableModel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
  * Created by 92474747 on 1/26/2016.
  */
 public class SemesterTable extends JPanel {
-    private JTable smstrTble;
     private SemesterTableModel semesterTableModel;
+    private JTable smstrTble;
 
     public SemesterTable() {
-        semesterTableModel = new SemesterTableModel();
+        try {
+            semesterTableModel = new SemesterTableModel();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         smstrTble = new JTable(semesterTableModel);
+        this.add(new JScrollPane(smstrTble), BorderLayout.CENTER);
+        smstrTble.setRowHeight(50);
+
     }
 
     void Refresh() {
--- src/aero/mahan/view/tables/models/SemesterTableModel.java
+++ src/aero/mahan/view/tables/models/SemesterTableModel.java
@@ -1,28 +1,37 @@
 package aero.mahan.view.tables.models;
 
+import aero.mahan.data.DAO.SemesterDAO;
 import aero.mahan.model.Semester;
 
+import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
 
 /**
  * Created by 92474747 on 1/26/2016.
  */
 
-public class SemesterTableModel extends AbstractTableModel  {
+public class SemesterTableModel extends AbstractTableModel {
 
     private String[] column = {"Semester Name", "Term"};
     private ArrayList<Semester> semesterArrayList;
 
-    public SemesterTableModel() {
-
-        semesterArrayList = new ArrayList<Semester>();
-
-    }
+    private SemesterDAO db;
 
     public void setSemesterArrayList(ArrayList<Semester> semesterList) {
         this.semesterArrayList = semesterList;
     }
+
+    public SemesterTableModel() throws SQLException {
+      //  semesterArrayList = new ArrayList<Semester>();
+        db = new SemesterDAO();
+        semesterArrayList = db.read();
+
+
+    }
+
 
     @Override
     public String getColumnName(int i) {
@@ -45,9 +54,52 @@ public class SemesterTableModel extends AbstractTableModel  {
     }
 
     @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
+    public Object getValueAt(int row, int col) {
+        for (int i = 0; i < semesterArrayList.size(); i++) {
 
+            Semester s = semesterArrayList.get(row);
+            switch (col) {
+                case 0:
+                    return s.getSemesterYear();
+                case 1:
+                    return s.getTermNo();
+
+
+
+                default:
+
+                    break;
+            }
+
+        }
         return null;
+
+    }
+
+    @Override
+    public void setValueAt(Object value, int row, int col) {
+        if (semesterArrayList == null) {
+            return;
+        }
+        Semester s = (Semester) semesterArrayList.get(row);
+
+        switch (col) {
+
+            case 0:
+                s.setSemesterYear((Integer) value);
+
+                break;
+
+            case 1:
+                s.setTermNo((Integer) value);
+
+                break;
+//            case 2: s.setSemesterId((Integer) value);
+//
+//                break;
+            default:
+                break;
+        }
 
     }
 
