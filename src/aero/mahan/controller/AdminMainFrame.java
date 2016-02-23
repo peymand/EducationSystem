3a38f20ae7f786312c3197522c9da161f24aca8e
921532 <shabnam822002@yahoo.com>
2016-02-23 08:44:23 +0330

    dispose login Form after click login button

--- src/aero/mahan/controller/AdminMainFrame.java
+++ src/aero/mahan/controller/AdminMainFrame.java
@@ -1,10 +1,14 @@
 package aero.mahan.controller;
 
 import aero.mahan.biz.BLO.SemesterBLO;
+import aero.mahan.data.DAO.SemesterDAO;
+import aero.mahan.model.Semester;
 import aero.mahan.view.panels.SemesterPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Created by 92474747 on 1/26/2016.
@@ -16,6 +20,7 @@ public class AdminMainFrame extends JFrame {
     JTabbedPane semesterTab;
     SemesterPanel semesterPanel;
     SemesterBLO semesterBLO;
+    ArrayList<Semester> semesters;
 
     public AdminMainFrame() {
         super("Administration Environment");
@@ -30,11 +35,23 @@ public class AdminMainFrame extends JFrame {
         semesterPanel = new SemesterPanel();
         semesterTab.insertTab("Semester", null, semesterPanel, null, 0);
         semesterTab.setTabPlacement(JTabbedPane.TOP);
-
-
+        semesterBLO = new SemesterBLO();
+        semesters = new ArrayList<>();
+        loadSemesterFromDB();
 
     }
 
+    public void loadSemesterFromDB(){
+        try {
+            semesters = semesterBLO.loadSemesters();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,e.getMessage());
+        }
+        setDataToSemesterTablePanel(semesters);
+    }
+    public void setDataToSemesterTablePanel(ArrayList<Semester> semesters){
+        semesterPanel.setSemesterArrayList(semesters);
+    }
 
 
 }
