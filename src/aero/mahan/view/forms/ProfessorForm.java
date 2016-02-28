32d704b7a49289123f16bb52ae89b17baecb3e5c
shanamgh <shabnam822002@yahoo.com>
2016-02-28 08:26:54 +0330

    complete professor form

--- src/aero/mahan/view/forms/ProfessorForm.java
+++ src/aero/mahan/view/forms/ProfessorForm.java
@@ -1,15 +1,9 @@
 package aero.mahan.view.forms;
 
-import aero.mahan.model.Professor;
-import aero.mahan.model.Semester;
-import aero.mahan.view.interfaces.IsemesterNotifier;
-
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * Created by 926365 on 2/22/2016.
@@ -48,8 +42,8 @@ public class ProfessorForm extends JPanel {
     public ProfessorForm() {
 
         Dimension dim = new Dimension();
-        dim.height = 200;
-        dim.width = 700;
+        dim.height = 300;
+        dim.width = 900;
         setPreferredSize(dim);
         setMinimumSize(dim);
         TitledBorder professorTitledBorder = new TitledBorder("Professor");
@@ -59,105 +53,97 @@ public class ProfessorForm extends JPanel {
 
         name = new JLabel("Name:");
         family = new JLabel("Family:");
-        password=new JLabel("Password:");
-        degree=new JLabel("Degree:");
-        professorNo=new JLabel("Professor No:");
+        password = new JLabel("Password:");
+        degree = new JLabel("Degree:");
+        professorNo = new JLabel("Professor No:");
 
 
         nameText = new JTextField(10);
         familyText = new JTextField(10);
         passwordText = new JTextField(10);
         degreeText = new JTextField(10);
-        professorNoText=new JTextField(10);
+        professorNoText = new JTextField(10);
+
 
         addBtn = new JButton("Add");
         saveBtn = new JButton("Save");
         deleteBtn = new JButton("Delete");
         editBtn = new JButton("Edit");
-
-//        addBtn.addActionListener(new ActionListener() {
-//
-//            @Override
-//            public void actionPerformed(ActionEvent event) {
-//
-//                Professor p = new Professor();
-//                p.setSemesterYear(Integer.parseInt(getAcademicYearText()));
-//                s.setTermNo(Integer.parseInt(getSemesterNoText()));
-//                isemesterNotifier.addEventOccurred(s);
-//            }
-//
-//        });
-
-//        editBtn.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//            }
-//        });
-//        saveBtn.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//                isemesterNotifier.saveEventOccurred(null);
-//            }
-//        });
-//        deleteBtn.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//            }
-//        });
-//        designComponentLayout();
-//
-//    }
-
-//    private void designComponentLayout() {
-//        GridBagConstraints c = putAcademicYearOnForm();
-//
-//    }
-
-//    public GridBagConstraints putAcademicYearOnForm() {
-//        GridBagConstraints c = new GridBagConstraints();
-//
-
-//        c.weighty = 0.05;
-//        c.anchor = GridBagConstraints.FIRST_LINE_START;
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//
-//        c.gridx = 0;
-//        c.gridy = 0;
-//        add(academicYear, c);
-//
-//        c.gridx = 0;
-//        c.gridy = 1;
-//        add(semesterNo, c);
-//
-//        c.gridx = 1;
-//        c.gridy = 0;
-//        add(academicYearText, c);
-//
-//        c.gridx = 1;
-//        c.gridy = 1;
-//        add(semesterNoText, c);
-//
-//        c.gridx = 0;
-//        c.gridy = 3;
-//        add(addBtn, c);
-//
-//        c.gridx = 1;
-//        c.gridy = 3;
-//        add(saveBtn, c);
-//
-//        c.gridx = 2;
-//        c.gridy = 3;
-//        add(deleteBtn, c);
-//
-//        c.gridx = 3;
-//        c.gridy = 3;
-//        add(editBtn, c);
-//
-//        return c;
+        designComponentLayout();
     }
+    private void designComponentLayout() {
+        GridBagConstraints c = putProfessorOnForm();
+
+    }
+
+    public GridBagConstraints putProfessorOnForm() {
+        GridBagConstraints c = new GridBagConstraints();
+
+
+        c.weighty = 0.001;
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        c.gridx = 0;
+        c.gridy = 0;
+        add(name, c);
+
+        c.gridx = 3;
+        c.gridy = 0;
+        add(family, c);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        add(password, c);
+
+        c.gridx = 3;
+        c.gridy = 1;
+        add(degree, c);
+
+        c.gridx = 0;
+        c.gridy = 2;
+        add(professorNo, c);
+
+        c.gridx = 1;
+        c.gridy = 0;
+        add(nameText, c);
+
+        c.gridx = 4;
+        c.gridy = 0;
+        add(familyText, c);
+
+        c.gridx = 1;
+        c.gridy = 1;
+        add(passwordText, c);
+
+        c.gridx = 4;
+        c.gridy = 1;
+        add(degreeText, c);
+
+        c.gridx = 1;
+        c.gridy = 2;
+        add(professorNoText, c);
+
+        c.gridx = 0;
+        c.gridy = 3;
+        add(addBtn , c);
+
+        c.gridx = 1;
+        c.gridy = 3;
+        add(saveBtn, c);
+
+        c.gridx = 2;
+        c.gridy = 3;
+        add(deleteBtn, c);
+
+        c.gridx = 3;
+        c.gridy = 3;
+        add(editBtn, c);
+
+        return c;
+
+    }
+
 
 }
 
