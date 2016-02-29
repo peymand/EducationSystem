565540b20c96dc852bb4ba2755b330013cef46e2
shanamgh <shabnam822002@yahoo.com>
2016-02-29 08:00:53 +0330

    nazanin created professor class in model package and edited person class.

--- src/aero/mahan/model/Professor.java
+++ src/aero/mahan/model/Professor.java
@@ -4,5 +4,36 @@ package aero.mahan.model;
  * Created by 92474747 on 1/26/2016.
  */
 public class Professor extends Person {
+    private int professorNo;
+    private int professorId;
+    private String degree;
+
+
+    public int getProfessorNo() {
+        return professorNo;
+    }
+
+    public void setProfessorNo(int professorNo) {
+        this.professorNo = professorNo;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public int getProfessorId() {
+        return professorId;
+    }
+
+    public void setProfessorId(int professorId) {
+        this.professorId = getProfessorId();
+    }
+
+
+
 
 }
