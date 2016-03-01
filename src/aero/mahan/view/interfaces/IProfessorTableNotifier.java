8ed29b69492966b0b9ad9669a2769619b426be4d
shanamgh <shabnam822002@yahoo.com>
2016-03-01 08:44:18 +0330

    Amir created IProfessorTableNotifier to Interface Package.

---
+++ src/aero/mahan/view/interfaces/IProfessorTableNotifier.java
@@ -0,0 +1,10 @@
+package aero.mahan.view.interfaces;
+
+import aero.mahan.model.Professor;
+
+/**
+ * Created by 926365 on 3/1/2016.
+ */
+public interface IProfessorTableNotifier {
+    void rowSelectionEventOccured(Professor value1);
+}
