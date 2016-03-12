0b74046f9ce2fbaf6d2cc62db153ec6b025f90ea
amir.gharavi <amir.gharavi@yahoo.com>
2016-03-12 12:30:59 +0330

    Custom Crud Panel

---
+++ src/aero/mahan/view/custompanel/CustomJPanel.java
@@ -0,0 +1,10 @@
+package aero.mahan.view.custompanel;
+
+import javax.swing.*;
+
+/**
+ * Created by DemoAccount on 3/12/2016.
+ */
+public abstract class CustomJPanel extends JPanel{
+    public abstract Object getInsertedData();
+}
