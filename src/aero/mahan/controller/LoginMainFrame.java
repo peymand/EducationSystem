dc92eadd446afe114e0128d59e4168024f247685
azmoon2012@gmail.com <azmoon2012@gmail.com>
2016-03-26 12:43:10 +0430

    test commit 3

--- src/aero/mahan/controller/LoginMainFrame.java
+++ src/aero/mahan/controller/LoginMainFrame.java
@@ -39,7 +39,6 @@ public class LoginMainFrame extends JFrame {
                 } catch (SQLException e) {
                     JOptionPane.showMessageDialog(null, e.getMessage());
                     JOptionPane.showMessageDialog(null,e.getMessage());
-                    JOptionPane.showMessageDialog(null,e.getMessage());
                 }
             }
         });
